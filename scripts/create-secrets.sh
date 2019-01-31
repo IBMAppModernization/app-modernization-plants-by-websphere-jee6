04d3a6210ceff163d64d0c87d64533bf5339ec9b
David Carew <carew@us.ibm.com>
2019-01-31 04:00:08 -0600

    Renamed web module to pbw-web

---
+++ scripts/create-secrets.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Enter MySQL username: "
+read -r username
+echo "Enter MySQL password: "
+read -r password
+echo "Enter MySQL host: "
+read -r host
+echo "Enter MySQL port: "
+read -r port
+
+ENC_username=$(echo "$username" | tr -d '\n' | base64)
+ENC_password=$(echo "$password" | tr -d '\n' | base64)
+ENC_host=$(echo "$host" | tr -d '\n' | base64)
+ENC_port=$(echo "$port" | tr -d '\n' | base64)
+
+cat <<EOF > k8s/pbw-mariadb-credentials.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: pbw-mariadb-credentials
+  labels:
+    app: pbw-mariadb
+type: Opaque
+data:
+  username: $ENC_username
+  password: $ENC_password
+  host: $ENC_host
+  port: $ENC_port
+EOF
---
+++ scripts/test-db-connection.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ $1 =~ '^(http(s)?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$' ]]; then
+  echo "Fatal error: Invalid URL format"
+  exit 1
+fi
+
+if [ ! -f k8s/pbw-mariadb-credentials.yaml ]; then
+  echo "Fatal error: Can't find k8s secrets file on path k8s/pbw-mariadb-credentials.yaml"
+  exit 1
+fi
+
+# store the whole response with the status at the and
+HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -H "Content-Type: text/plain" -d @k8s/pbw-mariadb-credentials.yaml $1)
+
+# extract the body
+HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
+# extract the status
+HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+if [ $HTTP_STATUS -ne 200 ]; then
+   if [ $HTTP_STATUS -eq 400 ] || [ $HTTP_STATUS -eq 401 ]; then
+      echo "Error validating parameters: try creating the secrets file again with the right host, port, username and password"
+      exit 1
+   else
+      echo "Fatal error:  invalid URL for Database connection validation service or service not available"
+      exit 1
+   fi
+fi
+
+# Validate the body
+if [ "$HTTP_BODY" == "Connection successful" ]; then
+  echo "Database connection successful"
+  exit 0
+else
+  echo "Fatal error:  invalid URL for database connection validation service"
+  exit 1
+fi
