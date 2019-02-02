5a9add7fa2a767dcf8b420f9d2e4198f5f688da8
David Carew <carew@us.ibm.com>
2019-02-02 06:18:23 -0600

    Renamed generated .ear

--- scripts/test-db-connection.sh
+++ scripts/test-db-connection.sh
@@ -5,13 +5,13 @@ if [[ $1 =~ '^(http(s)?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]
   exit 1
 fi
 
-if [ ! -f k8s/pbw-mariadb-credentials.yaml ]; then
-  echo "Fatal error: Can't find k8s secrets file on path k8s/pbw-mariadb-credentials.yaml"
+if [ ! -f k8s/pbw-mysql-credentials.yaml ]; then
+  echo "Fatal error: Can't find k8s secrets file on path k8s/pbw-mysql-credentials.yaml"
   exit 1
 fi
 
 # store the whole response with the status at the and
-HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -H "Content-Type: text/plain" -d @k8s/pbw-mariadb-credentials.yaml $1)
+HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -H "Content-Type: text/plain" -d @k8s/pbw-mysql-credentials.yaml $1)
 
 # extract the body
 HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
