7175a210a6bf4aa23c8420a169f7a350e8d88ff6
David Carew <carew@us.ibm.com>
2019-02-02 18:06:56 -0600

    Debugging errors in web app

--- pbw-web/src/main/java/com/ibm/websphere/samples/pbw/jpa/OrderItem.java
+++ pbw-web/src/main/java/com/ibm/websphere/samples/pbw/jpa/OrderItem.java
@@ -102,7 +102,7 @@ public class OrderItem
 	@JoinColumn(name="INVENTORYID", insertable=false, updatable=false)
 	private Inventory inventory;
 	@ManyToOne
-	@JoinColumn(name="ORDER_ORDERID")
+	@JoinColumn(name="ORDER_ORDERID",  insertable=false, updatable=false)
 	private Order order;
 
 	public int getCategory() {
