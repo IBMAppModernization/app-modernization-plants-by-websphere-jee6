f85a6f693fe869064aa8a620b7b14907e4759fe4
David Carew <carew@us.ibm.com>
2019-10-08 15:28:48 -0500

    Declare EJB in JSF bean instead of inject

--- pbw-web/src/main/java/com/ibm/websphere/samples/pbw/war/ShoppingBean.java
+++ pbw-web/src/main/java/com/ibm/websphere/samples/pbw/war/ShoppingBean.java
@@ -1,15 +1,15 @@
 //
-// COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, 
-// modify, and distribute these sample programs in any form without payment to IBM for the purposes of 
-// developing, using, marketing or distributing application programs conforming to the application 
-// programming interface for the operating platform for which the sample code is written. 
-// Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS 
-// AND IBM DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED 
-// WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE, 
-// TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE 
-// SAMPLE SOURCE CODE. IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS 
-// OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.  
+// COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy,
+// modify, and distribute these sample programs in any form without payment to IBM for the purposes of
+// developing, using, marketing or distributing application programs conforming to the application
+// programming interface for the operating platform for which the sample code is written.
+// Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS
+// AND IBM DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE,
+// TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE
+// SAMPLE SOURCE CODE. IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS
+// OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
 //
 // (C) COPYRIGHT International Business Machines Corp., 2001,2011
 // All Rights Reserved * Licensed Materials - Property of IBM
@@ -61,7 +61,7 @@ public class ShoppingBean implements Serializable {
 	private LinkedList<ProductBean> products;
 	private float shippingCost;
 
-	@Inject
+	@EJB
 	private ShoppingCartBean shoppingCart;
 
 	public String performAddToCart () {
@@ -93,7 +93,7 @@ public class ShoppingBean implements Serializable {
 	}
 
 	public String performRecalculate () {
-		
+
 		shoppingCart.removeZeroQuantityItems();
 
 		this.cartItems = wrapInventoryItems(shoppingCart.getItems());
@@ -179,7 +179,7 @@ public class ShoppingBean implements Serializable {
 	 public ShoppingCartBean getCart() {
 		 return shoppingCart;
 	 }
-	 
+
 	 private ArrayList<ShoppingItem> wrapInventoryItems(Collection<Inventory> invItems) {
 		 ArrayList<ShoppingItem> shoppingList = new ArrayList<ShoppingItem>();
 		 for (Inventory i : invItems) {
